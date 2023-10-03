eb6b5c8762564347d2d3d0c42fc6f65d03376f8a
acrisanto <alanvcrisanto@gmail.com>
2023-10-02 21:12:15 -0500

    first commit

---
+++ src/testjenkinscicd/TestJenkinsCICD.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testjenkinscicd;
+
+/**
+ *
+ * @author Crisvall
+ */
+public class TestJenkinsCICD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo, prueba de CI/CD con Jenkins");
+    }
+    
+}
